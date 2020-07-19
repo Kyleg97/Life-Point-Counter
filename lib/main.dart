aed8a0f5b4f693168d18f7454656a6d6bb839917
Kyle <kyle.gilbert1622@gmail.com>
2020-07-19 18:33:22 -0400

    Updated key sizes, colors, fab, etc.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,13 +30,20 @@ class _MyHomePageState extends State<MyHomePage> {
   int player1 = 8000, player2 = 8000;
   bool selected = true; // true == player1, false == player2
   bool add = false; // true == addition, false == subtraction
-  String numberToChange = "00000";
+  bool willEnd = false; // will this attack finish the duel?
+  bool dialVisible = true; // speed dial
+  Color currentColor = Colors.red;
+  TextEditingController controller = new TextEditingController();
+  String numberToChange = "";
+
+  void setDialVisible(bool value) {
+    setState(() {
+      dialVisible = value;
+    });
+  }
 
   void _calculate() {
-    int number = 0;
-    for (int i = 0; i < numberToChange.length; i++) {
-      number += int.parse(numberToChange[i]);
-    }
+    int number = int.parse(controller.text);
     setState(() {
       if (selected) {
         if (add) {
@@ -52,18 +59,34 @@ class _MyHomePageState extends State<MyHomePage> {
         }
       }
     });
-    numberToChange = "";
+    controller.text = "";
   }
 
   Widget _numberButton(int number) {
     return MaterialButton(
-      height: 100,
-      child: Text(number.toString(),
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-      textColor: Colors.black,
-      color: Colors.grey[100],
-      onPressed: () => numberToChange += number.toString(),
-    );
+        height: 100,
+        child: Text(number.toString(),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+        textColor: Colors.black,
+        color: Colors.grey[100],
+        onPressed: () {
+          setState(() {
+            controller.text += number.toString();
+            if (selected) {
+              if ((player1 - int.parse(controller.text)) <= 0) {
+                willEnd = true;
+              } else {
+                willEnd = false;
+              }
+            } else {
+              if ((player2 - int.parse(controller.text)) <= 0) {
+                willEnd = true;
+              } else {
+                willEnd = false;
+              }
+            }
+          });
+        });
   }
 
   Widget _operationButton(String operation) {
@@ -72,22 +95,75 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Text(operation,
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
         textColor: Colors.black,
-        color: operation == "+" ? Colors.blue[100] : Colors.red[100],
+        color: operation == "+" ? Colors.blue : Colors.red,
         //onPressed: () => operation == "+" ? add = true : add = false,
         onPressed: () {
           setState(() {
             if (operation == "+") {
               add = true;
+              currentColor = Colors.blue;
             } else {
               add = false;
+              currentColor = Colors.red;
             }
           });
         });
   }
 
+  SpeedDial buildSpeedDial() {
+    return SpeedDial(
+      animatedIcon: AnimatedIcons.menu_close,
+      animatedIconTheme: IconThemeData(size: 22.0, color: Colors.black),
+      backgroundColor: Colors.white,
+      // child: Icon(Icons.add),
+      onOpen: () => print('OPENING DIAL'),
+      onClose: () => print('DIAL CLOSED'),
+      visible: dialVisible,
+      curve: Curves.decelerate,
+      // overlayOpacity: 0.5,
+      children: [
+        SpeedDialChild(
+          child: Icon(Icons.rotate_left, color: Colors.white),
+          backgroundColor: Colors.deepOrange,
+          onTap: () => print('FIRST CHILD'),
+          label: 'Coin Toss',
+          labelStyle:
+              TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
+          labelBackgroundColor: Colors.deepOrangeAccent,
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.offline_pin, color: Colors.white),
+          backgroundColor: Colors.green,
+          onTap: () => print('SECOND CHILD'),
+          label: 'Dice Roll',
+          labelStyle:
+              TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
+          labelBackgroundColor: Colors.green,
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.restore, color: Colors.white),
+          backgroundColor: Colors.blue,
+          onTap: () => print('THIRD CHILD'),
+          label: 'Reset Game',
+          labelStyle:
+              TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
+          labelBackgroundColor: Colors.blue,
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
+      floatingActionButton: buildSpeedDial(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -95,75 +171,152 @@ class _MyHomePageState extends State<MyHomePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Text("Player 1"),
-                    Text(player1.toString()),
-                  ],
+                InkWell(
+                  onTap: () {
+                    if (!selected) {
+                      setState(() {
+                        selected = !selected;
+                      });
+                    }
+                  },
+                  child: Card(
+                    color: selected ? currentColor : Colors.white,
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: Column(
+                        children: <Widget>[
+                          Text(player1.toString(),
+                              style: TextStyle(
+                                fontSize: 42,
+                              )),
+                          Text("Player 1", style: TextStyle(fontSize: 18)),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-                Column(
-                  children: <Widget>[
-                    Text("Player 2"),
-                    Text(player2.toString()),
-                  ],
+                InkWell(
+                  onTap: () {
+                    if (selected) {
+                      setState(() {
+                        selected = !selected;
+                      });
+                    }
+                  },
+                  child: Card(
+                    color: !selected ? currentColor : Colors.white,
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: Column(
+                        children: <Widget>[
+                          Text(player2.toString(),
+                              style: TextStyle(
+                                fontSize: 42,
+                              )),
+                          Text("Player 2", style: TextStyle(fontSize: 18)),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 5),
             Container(
-              width: 260,
+              width: 300,
               height: 50,
-              decoration: BoxDecoration(
-                  border: Border.all(
-                      color: add == true ? Colors.blue : Colors.red, width: 1)),
-              child: Center(
-                child: Text(numberToChange),
+              child: TextFormField(
+                style: TextStyle(fontSize: 22, color: Colors.white),
+                textAlign: TextAlign.end,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                      borderSide: BorderSide(color: Colors.blue)),
+                  hintText: '',
+                ),
+                enabled: false,
+                controller: controller,
               ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    _numberButton(7),
-                    _numberButton(8),
-                    _numberButton(9),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    _numberButton(4),
-                    _numberButton(5),
-                    _numberButton(6),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    _numberButton(1),
-                    _numberButton(2),
-                    _numberButton(3),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    _operationButton("+"),
-                    _operationButton("-"),
-                    _numberButton(0),
-                  ],
-                ),
-              ],
+            Container(
+              width: width,
+              height: height / 2.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      _numberButton(7),
+                      _numberButton(8),
+                      _numberButton(9),
+                      MaterialButton(
+                          height: 100,
+                          child: Icon(Icons.backspace),
+                          textColor: Colors.black,
+                          color: Colors.grey[100],
+                          onPressed: () {
+                            setState(() {
+                              if (controller.text.length != 0) {
+                                controller.text = controller.text
+                                    .substring(0, controller.text.length - 1);
+                              }
+                            });
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      _numberButton(4),
+                      _numberButton(5),
+                      _numberButton(6),
+                      MaterialButton(
+                          height: 100,
+                          textColor: Colors.black,
+                          color: Colors.grey[100],
+                          onPressed: () {}),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      _numberButton(1),
+                      _numberButton(2),
+                      _numberButton(3),
+                      MaterialButton(
+                          height: 100,
+                          textColor: Colors.black,
+                          color: Colors.grey[100],
+                          onPressed: () {}),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      _operationButton("+"),
+                      _numberButton(0),
+                      _operationButton("-"),
+                      MaterialButton(
+                          height: 100,
+                          textColor: Colors.black,
+                          color: Colors.grey[100],
+                          onPressed: () {}),
+                    ],
+                  ),
+                ],
+              ),
             ),
             SizedBox(height: 25),
             MaterialButton(
-              height: 100,
-              child: Text("=",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+              height: height / 12,
+              minWidth: width / 1.2,
+              child: Text(willEnd ? "SEND TO THE SHADOW REALM" : "=",
+                  style: TextStyle(
+                      fontWeight: willEnd ? FontWeight.normal : FontWeight.bold,
+                      fontSize: willEnd ? 17 : 24)),
               textColor: Colors.black,
-              color: Colors.grey[100],
+              color: willEnd ? Colors.purple[500] : Colors.grey[100],
               onPressed: _calculate,
             ),
           ],
